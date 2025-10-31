16c1b87007efafd52fb5036de65b9a1990dd732b
tsmorz <tonysmoragiewicz@gmail.com>
2025-10-31 11:15:47 +0100

    bug fix when creating logger levels list

--- src/change_me/__main__.py
+++ src/change_me/__main__.py
@@ -8,18 +8,18 @@ from change_me.definitions import DEFAULT_LOG_LEVEL, LogLevel
 from change_me.utils import setup_logger
 
 
-def main(log_level: str) -> None:
+def main(log_level: str = DEFAULT_LOG_LEVEL) -> None:
     """Run the pipeline."""
     setup_logger(filename="log_file", log_dir=None, log_level=log_level)
     logger.info("Hello world!")
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--log-level",
         default=DEFAULT_LOG_LEVEL,
-        choices=list(LogLevel),
+        choices=list(LogLevel()),
         help="Log level.",
     )
     args = parser.parse_args()
---
+++ tests/main_test.py
@@ -0,0 +1,10 @@
+"""Test the main program."""
+
+from change_me.__main__ import main
+
+
+def test_main():
+    """Test the main function."""
+    result = main()
+
+    assert result is None
